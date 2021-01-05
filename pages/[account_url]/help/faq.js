1bc8f3c67d329ccaafe83250c67bce60025e9198
Aljazair Lindan <zairvai@Aljazairs-MacBook-Pro.local>
2021-01-05 09:21:54 +0700

    faq 1

--- pages/[account_url]/help/faq.js
+++ pages/[account_url]/help/faq.js
@@ -26,10 +26,24 @@ const PageFaq = props => {
 							<p>Sebuah konsep portal guna membantu memberikan informasi mengenai pesantren kepada masyarakat luas serta membantu pesantren untuk menyebarkan pandangan dan keilmuan mereka melalui artikel dan diskusi yang difasilitasi portal tersebut.</p>
 						</Panel>
 						<Panel header="Bagaimana cara login digipress" key="2">
-							<p>Dengan membuka halaman https://digipress.id/[nama_akun]/auth/login</p>
-							<p>[nama_akun] adalah nama unik URL untuk setiap pesantren yang telah didaftarkan oleh Admin Bakti Kominfo. Nama tersebut</p>
+							<p>Dengan membuka halaman https://digipress.id/[nama_akun]</p>
+							<p>[nama_akun] adalah nama unik URL untuk setiap pesantren yang telah didaftarkan oleh Admin Bakti Kominfo.</p>
 						</Panel>
-						
+						<Panel header="Kenapa harus dengan alamat email yang benar" key="3">
+							<p>Dengan memasukan alamat email yang benar, maka sistem digipress dapat melindungi anda dari lupa password dan kendala dalam menggunakan fiturnya.</p>
+						</Panel>
+						<Panel header="Kenapa saya harus verifikasi alamat email" key="4">
+							<p>Verifikasi email diperlukan sebagai standar keamanan data pengguna. Pada saat lupa password, hanya email terverifikasi yang dapat melakukan  perbaikan password.</p>
+						</Panel>
+						<Panel header="Bagaimana melakukan verifikasi alamat email" key="5">
+							<p>Setelah login, akan ada tanda bell nofitikasi di kanan atas, silahkan klik tanda tersebut dan klik tautan verifikasi email. dan selanjutnya ikuti proses yang diarahkan.</p>
+						</Panel>
+						<Panel header="Apa yang harus dilakukan saat lupa password" key="6">
+							<p>Pada form login, silahkan klik tautan 'lupa password' lalu masukan alamat email yang sudah diverifikasi. Selanjutnya kode keamanan untuk mengganti password dikirim ke alamat email tersebut. Lalu masukan kode tersebut pada saat penggantian password.</p>
+						</Panel>
+						<Panel header="Kode penggantian password tidak sampai ke alamat email" key="7">
+							<p>Kode keamanan mungkin bisa langsung masuk kotak 'inbox'. Jika tidak ada silahkan cek juga di kotak 'spam'</p>
+						</Panel>
 				</Collapse>
 				</Col>
 			</Row>
